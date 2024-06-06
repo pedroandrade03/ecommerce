c9a3c4eeb913ded807b6d06c7febfa13b30133dd
Pedro Andrade <pedrogamerp@gmail.com>
2024-06-06 14:07:11 -0300

    feat: Add CategoryCard and FeaturedCategory components

---
+++ frontend/src/components/CategoryCard.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+interface CategoryCardProps {
+  title: string;
+  itemCount: number;
+  backgroundColor: string;
+  imageSrc: string;
+  className?: string;
+}
+
+export function CategoryCard({ title, itemCount, backgroundColor, imageSrc, className }: CategoryCardProps) {
+  return (
+    <Card className={cn("hover:border hover:border-green-500 hover:text-green-500", backgroundColor, className)}>
+      <CardContent className="flex justify-center items-center p-5">
+        <div className="transform transition-transform duration-300 hover:scale-110">
+          <Image src={imageSrc} width={64} height={64} alt="Ícone de categoria" />
+        </div>
+      </CardContent>
+      <CardFooter className="flex flex-col items-center">
+        <p className="font-medium text-center text-nowrap">{title}</p>
+        <p className="font-light text-gray-900 text-nowrap">{itemCount} Items</p>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ frontend/src/components/FeaturedCategory.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { CategoryCard } from "./CategoryCard";
+
+const mockCategories = [
+  { title: "Cake & Milk", itemCount: 26, backgroundColor: "bg-green-50", imageSrc: "/categories/cat-13.png" },
+  { title: "Organic Kiwi", itemCount: 37, backgroundColor: "bg-amber-50", imageSrc: "/categories/cat-12.png" },
+  { title: "Peach", itemCount: 37, backgroundColor: "bg-yellow-50", imageSrc: "/categories/cat-11.png" },
+  { title: "Red Apple", itemCount: 37, backgroundColor: "bg-red-50", imageSrc: "/categories/cat-9.png" },
+  { title: "Cake & Milk", itemCount: 26, backgroundColor: "bg-green-50", imageSrc: "/categories/cat-13.png" },
+  { title: "Organic Kiwi", itemCount: 37, backgroundColor: "bg-amber-50", imageSrc: "/categories/cat-12.png" },
+  { title: "Peach", itemCount: 37, backgroundColor: "bg-yellow-50", imageSrc: "/categories/cat-11.png" },
+  { title: "Red Apple", itemCount: 37, backgroundColor: "bg-red-50", imageSrc: "/categories/cat-9.png" },
+  { title: "Organic Kiwi", itemCount: 37, backgroundColor: "bg-amber-50", imageSrc: "/categories/cat-12.png" },
+  { title: "Peach", itemCount: 37, backgroundColor: "bg-yellow-50", imageSrc: "/categories/cat-11.png" },
+  { title: "Peach", itemCount: 37, backgroundColor: "bg-yellow-50", imageSrc: "/categories/cat-11.png" },
+];
+
+export default function FeaturedCategory() {
+  return (
+    <div className="bg-gray-200 flex flex-col justify-center items-center p-16">
+      <div className="w-full flex content-start my-4">
+        <p className="text-4xl font-medium">Categorias em Destaque</p>
+      </div>
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full"
+      >
+        <CarouselContent>
+          {mockCategories.map((category, index) => (
+            <CarouselItem key={index} className="md:basis-1/4 lg:basis-1/10">
+              <CategoryCard
+                title={category.title}
+                itemCount={category.itemCount}
+                backgroundColor={category.backgroundColor}
+                imageSrc={category.imageSrc}
+                className="min-h-full flex flex-col"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
