536a18e34b153edfbe69bac63e51b96ca6148d4a
Pedro Andrade <pedrogamerp@gmail.com>
2024-06-16 19:04:17 -0300

    fix(backend): fix image serializer

--- backend/apps/products/serializers/product_image.py
+++ backend/apps/products/serializers/product_image.py
@@ -3,6 +3,13 @@ from apps.products import models
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = models.ProductImage
         fields = ('id', 'image', 'caption', 'is_primary')
+
+    def get_image(self, obj):
+        if obj.image:
+            return "http://127.0.0.1:8000" + obj.image.url
+        return None
